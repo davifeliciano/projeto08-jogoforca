6ec4970877b24d5a21dcc3ecc7edea112dd71d07
Davi Feliciano <dfeliciano37@gmail.com>
2023-02-23 18:34:35 -0300

    Usando global style para reset do CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 import { useState } from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Chute from "./components/Chute";
 import Jogo from "./components/Jogo";
 import Letras from "./components/Letras";
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+  }
+
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  #root,
+  #__next {
+    isolation: isolate;
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+`;
 
 const Container = styled.div`
   display: flex;
@@ -24,42 +72,45 @@ export default function App() {
   const [failCount, setFailCount] = useState(0);
 
   return (
-    <Container>
-      <Jogo
-        setGameActive={setGameActive}
-        wordArray={wordArray}
-        setWordArray={setWordArray}
-        unaccentedWordArray={unaccentedWordArray}
-        setUnaccentedWordArray={setUnaccentedWordArray}
-        guessedLetters={guessedLetters}
-        setGuessedLetters={setGuessedLetters}
-        setInputValue={setInputValue}
-        wordGuessed={wordGuessed}
-        setWordGuessed={setWordGuessed}
-        failCount={failCount}
-        setFailCount={setFailCount}
-      />
-      <Letras
-        gameActive={gameActive}
-        unaccentedWordArray={unaccentedWordArray}
-        guessedLetters={guessedLetters}
-        setGuessedLetters={setGuessedLetters}
-        failCount={failCount}
-        setFailCount={setFailCount}
-      />
-      <Chute
-        gameActive={gameActive}
-        setGameActive={setGameActive}
-        wordArray={wordArray}
-        unaccentedWordArray={unaccentedWordArray}
-        guessedLetters={guessedLetters}
-        setGuessedLetters={setGuessedLetters}
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        setWordGuessed={setWordGuessed}
-        failCount={failCount}
-        setFailCount={setFailCount}
-      />
-    </Container>
+    <>
+      <GlobalStyle />
+      <Container>
+        <Jogo
+          setGameActive={setGameActive}
+          wordArray={wordArray}
+          setWordArray={setWordArray}
+          unaccentedWordArray={unaccentedWordArray}
+          setUnaccentedWordArray={setUnaccentedWordArray}
+          guessedLetters={guessedLetters}
+          setGuessedLetters={setGuessedLetters}
+          setInputValue={setInputValue}
+          wordGuessed={wordGuessed}
+          setWordGuessed={setWordGuessed}
+          failCount={failCount}
+          setFailCount={setFailCount}
+        />
+        <Letras
+          gameActive={gameActive}
+          unaccentedWordArray={unaccentedWordArray}
+          guessedLetters={guessedLetters}
+          setGuessedLetters={setGuessedLetters}
+          failCount={failCount}
+          setFailCount={setFailCount}
+        />
+        <Chute
+          gameActive={gameActive}
+          setGameActive={setGameActive}
+          wordArray={wordArray}
+          unaccentedWordArray={unaccentedWordArray}
+          guessedLetters={guessedLetters}
+          setGuessedLetters={setGuessedLetters}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          setWordGuessed={setWordGuessed}
+          failCount={failCount}
+          setFailCount={setFailCount}
+        />
+      </Container>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./assets/css/reset.css";
-import "./assets/css/main.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
